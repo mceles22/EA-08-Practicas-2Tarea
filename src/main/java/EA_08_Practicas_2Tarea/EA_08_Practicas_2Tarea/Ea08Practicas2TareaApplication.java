b8800d4d5f208a88409b3876a897e34305169b93
mceles22 <133931561+mceles22@users.noreply.github.com>
2023-06-08 14:28:30 -0500

    Trabajo Acumulativo de semana 1 a la 3

---
+++ src/main/java/EA_08_Practicas_2Tarea/EA_08_Practicas_2Tarea/Ea08Practicas2TareaApplication.java
@@ -0,0 +1,13 @@
+package EA_08_Practicas_2Tarea.EA_08_Practicas_2Tarea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ea08Practicas2TareaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ea08Practicas2TareaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/EA_08_Practicas_2Tarea/EA_08_Practicas_2Tarea/Ea08Practicas2TareaApplicationTests.java
@@ -0,0 +1,13 @@
+package EA_08_Practicas_2Tarea.EA_08_Practicas_2Tarea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ea08Practicas2TareaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
